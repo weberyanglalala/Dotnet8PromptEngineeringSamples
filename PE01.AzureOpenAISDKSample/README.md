0a8bae6a04a5eb4011f1f4e511580ca4257b5376
Weber Yang <weberyanglalala@gmail.com>
2024-06-17 18:24:04 +0800

    feat(PE01): basic chat completion code sample

---
+++ PE01.AzureOpenAISDKSample/README.md
@@ -0,0 +1,9 @@
+# Azure.AI.OpenAI SDK
+## References
+- https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/openai/Azure.AI.OpenAI
+- https://learn.microsoft.com/en-us/dotnet/api/overview/azure/ai.openai-readme?view=azure-dotnet-preview
+## Installation
+
+```
+dotnet add package Azure.AI.OpenAI
+```
---
+++ PE01.PrincipleAndGuidelines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ClientModel;
+using OpenAI.Chat;
+
+namespace PE01.PrincipleAndGuidelines;
+
+class Program
+{
+    private static readonly string _openApiKey = "sk-????";
+
+    static async Task Main(string[] args)
+    {
+        var result = await GetCompletion("what is the capital of Japan?");
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> GetCompletion(string prompt, string model = "gpt-4o")
+    {
+        ChatClient client = new(model, _openApiKey);
+        ChatCompletion chatCompletion = await client.CompleteChatAsync(prompt);
+        return $"{chatCompletion}";
+    }
+    
+    private static async Task GetChatCompletionSteaming(string source)
+    {
+        try
+        {
+            var prompt = $"{source}";
+            ChatClient client = new(model: "gpt-4o", _openApiKey);
+            AsyncResultCollection<StreamingChatCompletionUpdate> updates
+                = client.CompleteChatStreamingAsync(prompt);
+            await foreach (StreamingChatCompletionUpdate update in updates)
+            {
+                foreach (ChatMessageContentPart updatePart in update.ContentUpdate)
+                {
+                    Console.Write(updatePart.Text);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unexpected error: {e.Message}");
+        }
+    }
+}
